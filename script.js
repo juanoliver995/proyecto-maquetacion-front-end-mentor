3aeb7239361621bd5dd3feb8b2c0a0f7a4bfdfad
Juan Oliver <juanoliver995@gmail.com>
2022-06-15 16:19:50 -0300

    second commit

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const features = document.getElementById('features');
 const arrowFeature = document.getElementById('arrow-features');
 const menuFeature = document.getElementById('menu-features');
-
+const company = document.getElementById('company');
+const arrowCompany = document.getElementById('arrow-company');
+const menuCompany = document.getElementById('menu-company');
+const menuClose = document.getElementById('menu-close');
+const menuMobile = document.getElementById('menu-mobile');
+const navMobile = document.getElementById('nav-mobile');
 features.addEventListener('click', function () {
     menuFeature.classList.toggle('active');
     arrowFeature.classList.toggle('rotate');
 })
 
-const company = document.getElementById('company');
-const arrowCompany = document.getElementById('arrow-company');
-const menuCompany = document.getElementById('menu-company');
+
 company.addEventListener('click', function () {
     menuCompany.classList.toggle('active');
     arrowCompany.classList.toggle('rotate');
+})
+
+menuClose.addEventListener('click', function () {
+    navMobile.classList.remove('active');
+})
+
+menuMobile.addEventListener('click', function () {
+    navMobile.classList.add('active');
 })
